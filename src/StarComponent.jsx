4218ac2b4a86869052703dca9b70f7a08a75cf39
Sukhendra Lodhi <mpct5jan1998@gmail.com>
2023-05-17 23:52:51 +0530

    Done

--- src/StarComponent.jsx
+++ src/StarComponent.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import Star from "./Star";
 
 const StarComponent = ({ maxStar, starRating, setStarRating }) => {
+    const [ starHover, setStarHover ] = useState(null);
     return (
         <div style={{ display: 'flex' }}>
             {Array.from({ length: maxStar }).map((_, index) => {
                 return (
                     <span
-                    
+                        onMouseEnter={() => setStarHover(index)}
+                        onMouseLeave={() => setStarHover(null)}
                         onClick={() => setStarRating(index + 1)}
                         key={index}>
-                        <Star filled={index < starRating} />
+                        <Star filled={ starHover !== null ? index <= starHover :  index < starRating} />
                     </span>
                 );
             })}
